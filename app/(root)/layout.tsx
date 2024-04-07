e73f420427b728952ee72395d1db31bb65644414
Khwaab Jaiswal <khwaabjaiswal2902@gmail.com>
2024-04-07 06:27:25 +0530

    First commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,26 @@
+import { redirect } from 'next/navigation';
+import { auth } from '@clerk/nextjs';
+
+import Navbar from '@/components/navbar'
+
+export default async function DashboardLayout({
+  children,
+  params
+}: {
+  children: React.ReactNode
+  params: { storeId: string }
+}) {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect('/sign-in');
+  }
+
+
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+};
